dc944c610ffe45a3a8026957e3d8443e74a475f4
illgonggu <rbfl2365@icloud.com>
2022-09-06 20:08:55 +0900

    Initial Commit

---
+++ JJangguRotateTestApp/JJangguRotateTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJangguRotateTestAppApp.swift
+//  JJangguRotateTestApp
+//
+//  Created by Loopy on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct JJangguRotateTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
